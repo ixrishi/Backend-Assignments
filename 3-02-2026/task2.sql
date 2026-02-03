2a15070115ed8b1518d95ce8ee7b5e6c11905348
Rishi Konga <rishi.konga@innovatechs.com>
2026-02-03 15:46:31 +0530

    task2 added

---
+++ 3-02-2026/task2.sql
@@ -0,0 +1,27 @@
+insert into  students values(1000000,'rishi','rishi@gmail.com',21,'2026-01-06',1);
+
+
+insert into  students values(1000000,'sahit','sahit@gmail.com',21,'2026-02-06',0);
+insert into  students values(1000001,'amogh','amogh@gmail.com',21,'2026-01-06',1);
+insert into  students values(1000002,'vishnu','vishnu@gmail.com',21,'2026-01-06',1);
+
+insert into  students values(1000003,'rishab','rishab@gmail.com',21,'2026-01-06',1);
+insert into  students values(1000004,'snehith','snehith@gmail.com',21,'2026-01-06',1);
+insert into  students values(1000005,'rahul','rahul@gmail.com',21,'2026-01-06',1);
+
+insert into  students values(1000006,'jay','jay@gmail.com',22,'2026-01-06',1);
+insert into  students values(1000007,'ramesh','ramesh@gmail.com',21,'2026-01-06',1);
+insert into  students values(1000009,'suresh','suresh@gmail.com',21,'2026-01-06',1);
+
+insert into courses values(1,'frontend',10000);
+insert into courses values(2,'backend',30000);
+insert into courses values(3,'ML',50000);
+insert into courses values(4,'AI',100000);
+insert into courses values(5,'mongodb',10000);
+insert into courses values(6,'sql',10000);
+insert into courses values(7,'react',10000);
+insert into courses values(8,'javascript',10000);
+insert into courses values(9,'java',10000);
+insert into courses values(10,'python',10000);
+
+insert into ENROLLMENTS values()
